7fdeda4e08b4171447dcf4eface6279e1a6194d8
micah-sanchez <mjsanch4@gmail.com>
2023-02-02 15:18:35 -0700

    Added submitManager and prop to send to RecipeCreate

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData"
+
+function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  const submitManager = (event) => {
+    event.preventDefault()
+    console.log("submitted")
+  }
+  
+  const handleCreate = () => {
+    console.log("clicked!")
+  }
+
+  // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
+  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+
+  
+  return (
+    <div className="App">
+      <header><h1>Delicious Food Recipes</h1></header>
+      <RecipeList />
+      <RecipeCreate submitManager={submitManager}/>
+    </div>
+  );
+}
+
+export default App;
