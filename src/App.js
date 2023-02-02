4b4555556fa2e9f6210d5680ffc6ce56b708acbd
micah-sanchez <mjsanch4@gmail.com>
2023-02-02 16:26:48 -0700

    removed formData, submitManager, change handler to this file

--- src/App.js
+++ src/App.js
@@ -7,31 +7,7 @@ import RecipeData from "./RecipeData"
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
 
-  const initialFormData = {
-    name: "",
-    cuisine: "",
-    photo: "",
-    ingredients: "",
-    preparation: "",
-  }
 
-  const [formData, setFormData] = useState({...initialFormData})
-
-  const submitManager = (event) => {
-    event.preventDefault();
-    setFormData({...initialFormData});
-    console.log(formData)
-
-      
-    console.log("submitted")
-  }
-  
-  const handleFormChange = ({target}) => {
-    setFormData({
-      ...formData,
-      [target.name]: target.value,
-    })
-  }
 
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
   // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
@@ -40,8 +16,8 @@ function App() {
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
-      <RecipeCreate submitManager={submitManager} handleFormChange={handleFormChange}/>
+      <RecipeList recipes={recipes}/>
+      <RecipeCreate recipes={recipes} setRecipes={setRecipes}/>
     </div>
   );
 }
