48ee898c1081fc82209bf53602f6e925c1226bb8
micah-sanchez <mjsanch4@gmail.com>
2023-02-02 15:08:19 -0700

    Added form fields HTML

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -13,7 +13,56 @@ function RecipeCreate() {
       <table>
         <tbody>
           <tr>
-            <td></td>
+          <td>
+            <label name="name">
+              <input
+              name="name"
+              id="name"
+              placeholder="Name"
+              type="text"
+              />
+            </label>
+          </td>
+          <td>
+            <label name="cuisine">
+              <input
+              name="cuisine"
+              id="cuisine"
+              placeholder="Cuisine"
+              type="text"
+              />
+            </label>
+          </td>
+          <td>
+            <label name="photo">
+              <input
+              name="photo"
+              id="photo"
+              placeholder="URL"
+              type="url"
+              />
+            </label>
+          </td>
+          <td>
+            <label name="ingredients">
+              <input
+              name="ingredients"
+              id="ingredients"
+              placeholder="Ingredients"
+              type="text"
+              />
+            </label>
+          </td>
+          <td>
+            <label name="preparation">
+              <input
+              name="preparation"
+              id="preparation"
+              placeholder="Preparation"
+              type="text"
+              />
+            </label>
+          </td>
             <td>
               <button type="submit">Create</button>
             </td>
